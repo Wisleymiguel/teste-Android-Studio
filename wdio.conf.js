f15fadba0d8be3d6495cdbb73020f66a38133145
Wisleymiguel <wisleymiguel4032x@gmail.com>
2025-04-02 22:20:36 -0300

    Versão adaptada para execução em nuvem

--- wdio.conf.js
+++ wdio.conf.js
@@ -1,57 +1,58 @@
 exports.config = {
-    
-    runner: 'local',
-    port: 4723,
-    
-    specs: [
-        './test/specs/**/*.js'
-    ],
-    
-    exclude: [
-   
-    ],
-    
-    maxInstances: 1,
-    
-    capabilities: [{
-        
-        platformName: 'Android',
-        'appium:deviceName': 'ebac-q',
-        'appium:platformVersion': '15.0',
-        'appium:automationName': 'UiAutomator2',
-        'appium:app': `${process.cwd()}/app/ebacshop.apks`,
-        'appium:appWaitActivity': 'MainActivity',
-        'appium:disableIdLocatorAutocompletion': true,
+  runner: 'local',
+  port: 4723,
 
-    }],
+  // Sauce Labs auth
+  user: 'oauth-wisleymiguel4032x-4019b',
+  key: 'a1a34a93-fd52-4f85-a4d1-c08246ef704e',
+  region: 'us',
 
-   
-    logLevel: 'info',
-    
-    bail: 0,
-    
-    waitforTimeout: 10000,
-   
-    connectionRetryTimeout: 120000,
-   
-    connectionRetryCount: 3,
-    
-    framework: 'mocha',
- reporters: [
-  'spec',
-  [
-    'allure',
+  specs: ['./test/specs/**/*.js'],
+  exclude: [],
+  maxInstances: 1,
+
+  capabilities: [
     {
-      outputDir: 'allure-results',
-      disableWebdriverStepsReporting: true,
-      disableWebdriverScreenshotsReporting: true,
-    },
+      platformName: 'Android',
+      'appium:deviceName': 'Android GoogleAPI Emulator',
+      'appium:platformVersion': '12.0',
+      'appium:automationName': 'UiAutomator2',
+      'appium:app': 'storage:filename=ebacshop (1).aab',
+      'appium:appWaitActivity': 'MainActivity',
+      'appium:disableIdLocatorAutocompletion': true,
+      'sauce:options': {
+        build: 'appium-build-teste-Ebac',
+        name: 'EBAC Shop Test',
+        deviceOrientation: 'PORTRAIT'
+      }
+    }
   ],
-],
-  
-mochaOpts: {
-  ui: 'bdd',
-  timeout: 60000
-},
 
-}
+  logLevel: 'info',
+  bail: 0,
+  waitforTimeout: 10000,
+  connectionRetryTimeout: 120000,
+  connectionRetryCount: 3,
+
+  // ATIVAR o Sauce Labs
+  services: ['sauce'],
+
+  framework: 'mocha',
+
+  reporters: [
+    'spec',
+    [
+      'allure',
+      {
+        outputDir: 'allure-results',
+        disableWebdriverStepsReporting: true,
+        disableWebdriverScreenshotsReporting: true,
+      },
+    ],
+  ],
+
+  mochaOpts: {
+    ui: 'bdd',
+    timeout: 60000
+  },
+};
